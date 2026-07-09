3576ecb109c92e910f76f430afc0ed41afcb3cf8
Siya Rana <siyaranaofficial26@gmail.com>
2026-07-09 21:18:15 +0530

    Refactor database.js for improved clarity and structure

--- database.js
+++ database.js
@@ -4,19 +4,17 @@ dotenv.config();
 
 
 const pool= mysql.createPool({
-    host: process.env.MYSQL_HOST,  //env variables ensure that sensitive information is not hardcoded into the codebase, enhancing security and flexibility.
+    host: process.env.MYSQL_HOST,  
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise();
-//print the entire table
+
 export async function getData(){
     const [rows]= await pool.query('SELECT * FROM users');
    return rows;
 }
-// const note = await  getData();
-// console.log(note);
-//now we'll try to get a singular user email by its id
+
 export async function getUserByEmail(email){
     const [rows]= await pool.query('SELECT * FROM users WHERE email=?',[email]);
     return rows[0];
@@ -27,7 +25,7 @@ export async function getUserById(id){
     return rows[0];
 }
 
-//noow we'll create a user
+
 export async function createUser(
     name,
     email,
@@ -47,7 +45,6 @@ export async function createUser(
     return result.insertId;
 }
 
-//update name, phone, age, bio
 export async function UpdateById(id, name, phone, age, bio,role){
     const [rows]= await pool.query('UPDATE users SET name= ?, phone=?, age= ?, bio= ? , role=? WHERE id= ?',[name, phone, age, bio,role,id]);
     return rows[0];
@@ -59,5 +56,3 @@ export async function UpdatePasswordById(id,hashedPassword){
     const [rows]= await pool.query('UPDATE users SET password= ? WHERE id= ?',[hashedPassword,id]);
     return rows[0];
 }
-//  const newUser = await createUser('mynewemail@example.com', 'newpassword');
-//  console.log(newUser);
